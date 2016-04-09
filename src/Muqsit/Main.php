e9e9845336de3c656e4c2d8d704e4fc7bff3ffc5
Muqsit Rayyan <hackhack05@gmail.com>
2016-04-09 16:35:52 +0300

    Initial commint

---
+++ src/Muqsit/Main.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Muqsit;
+
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\block\BlockBreakEvent;
+
+class Main extends PluginBase implements Listener{
+/** @var array $breaks */
+private $breaks;
+
+// Thanks to @PrimusLV
+public function e_block_break(BlockBreakEvent $event){
+   if($event->isCancelled()) return;
+   $name = strtolower(trim($event->getPlayer()->getName()));
+   $this->breaks[]);
+   if($this->breaks[$name] >= 128){
+      $event->getPlayer()->sendMessage("You broke 128 blocks!"));
+   }
+}
